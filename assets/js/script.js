e9e6bcb4c4ba0ce94d769214d078875ff746e483
Anrkae <barretosportwear@gmail.com>
2025-03-20 20:12:36 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,15 @@
 // Função para alternar a visibilidade da sidebar
 function toggleSidebar() {
     const sidebar = document.querySelector('.sidebar');
-    // Adiciona ou remove a classe 'open' para abrir/fechar a sidebar
     sidebar.classList.toggle('open');
 }
 
 // Função para fechar a sidebar se o usuário clicar fora dela
 document.addEventListener('click', function(event) {
     const sidebar = document.querySelector('.sidebar');
-    const sidebarToggle = document.querySelector('.sidebar-toggle');
+    const menuIcon = document.querySelector('.menu-icon');
 
-    // Verifica se o clique foi fora da sidebar e do botão de alternância
-    if (!sidebar.contains(event.target) && !sidebarToggle.contains(event.target)) {
+    if (!sidebar.contains(event.target) && !menuIcon.contains(event.target)) {
         sidebar.classList.remove('open');
     }
 });
