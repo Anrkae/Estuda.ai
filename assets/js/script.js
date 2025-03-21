db99f88863de1cb493033ffe1690656ca0c30aaf
Anrkae <barretosportwear@gmail.com>
2025-03-21 11:43:47 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ let cronometro;
 
 const questoesEl = document.getElementById('questoes');
 const acertosEl = document.getElementById('acertos');
+const acertosBar = document.getElementById('acertos-bar');
+const errosBar = document.getElementById('erros-bar');
 
 // Funções de Timer
 function startTimer() {
@@ -46,65 +48,18 @@ function registrarQuestoes() {
 
   questoesEl.textContent = questoesResolvidas;
   acertosEl.textContent = acertos;
-  atualizarGrafico();
+  atualizarBarras();
 }
 
-// Funções de Data
-function setDateRange(range) {
-  let startDate = '';
-  let endDate = '';
-  const now = new Date();
-
-  switch (range) {
-    case '24h':
-      startDate = new Date(now.getTime() - 24 * 60 * 60 * 1000);
-      endDate = now;
-      break;
-    case '7d':
-      startDate = new Date(now.getTime() - 7 * 24 * 60 * 60 * 1000);
-      endDate = now;
-      break;
-    case '30d':
-      startDate = new Date(now.getTime() - 30 * 24 * 60 * 60 * 1000);
-      endDate = now;
-      break;
-  }
-
-  document.getElementById('startDate').value = startDate.toISOString().split('T')[0];
-  document.getElementById('endDate').value = endDate.toISOString().split('T')[0];
-}
-
-// Inicialização do gráfico com Chart.js
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-  type: 'bar', // Tipo de gráfico
-  data: {
-    labels: ['Acertos', 'Erros'], // Labels para o gráfico
-    datasets: [{
-      label: 'Desempenho',
-      data: [0, 0], // Dados iniciais de acertos e erros
-      backgroundColor: ['#4caf50', '#f44336'], // Cores
-      borderColor: ['#4caf50', '#f44336'],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    indexAxis: 'y', // Barra horizontal
-    responsive: true,
-    scales: {
-      x: {
-        beginAtZero: true
-      }
-    }
-  }
-});
-
-// Atualiza o gráfico quando as questões são registradas
-function atualizarGrafico() {
+// Atualiza as barras de acertos e erros
+function atualizarBarras() {
   // Calcular a quantidade de erros
   let erros = questoesResolvidas - acertos;
 
-  // Atualizar o gráfico
-  myChart.data.datasets[0].data = [acertos, erros];
-  myChart.update();
+  // Atualizar as larguras das barras de progresso
+  const percentualAcertos = (acertos / questoesResolvidas) * 100 || 0;
+  const percentualErros = (erros / questoesResolvidas) * 100 || 0;
+
+  acertosBar.style.width = `${percentualAcertos}%`;
+  errosBar.style.width = `${percentualErros}%`;
 }
